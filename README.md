4a76d0969998fa1723160c853d60beb14d8f366c
eberhardtj <30794167+eberhardtj@users.noreply.github.com>
2018-11-08 12:55:51 +0100

    init

---
+++ README.md
@@ -0,0 +1,86 @@
+# CultureGraph Config Set
+
+  * [Quickstart](#quickstart)
+  * [Configure](#configure)
+  * [Installation](#installation)
+    + [Modify solrconfig.xml](#modify-solrconfigxml)
+    + [Install Add-ons](#install-add-ons)
+  * [Automatic Installation](#automatic-installation)
+
+The Apache Solr configuration for Culturegraph.
+
+## Quickstart
+
+```
+# Start solr with 1GB Memory on Port 1111 and be verbose
+solr start -m 1g -p 1111 -V
+
+# Create core test with culturegraph config set
+solr create_core -c test -d culturegraph -p 1111 -V
+
+# Configure the data import handler for the core
+cd solr/CORE/conf
+nano solr-data-config.xml
+# Change fileName, baseDir or jdbc
+
+# Stop solr
+solr stop -all
+```
+
+## Configure
+
+Change the locations or data sources in `solr-data-config.xml`.
+
+## Installation
+
+**NOTE**: The config set contains a vanilla `solrconfig.xml` that needs to be modified.
+
+### Modify solrconfig.xml
+
+Add a */dataimport* request handler to `solrconfig.xml` (Location: `configsets/culturegraph`)
+
+```
+<lib dir="${solr.install.dir:../../../..}/dist/" regex="solr-dataimporthandler-.*\.jar" />
+```
+
+```
+  <requestHandler name="/dataimport" class="solr.DataImportHandler">
+    <lst name="defaults">
+      <str name="config">solr-data-config.xml</str>
+    </lst>
+  </requestHandler>
+```
+
+### Install Add-ons
+
+Install the following add-ons:
+
+* https://github.com/culturegraph/solr-hashcode-transformer
+* https://github.com/culturegraph/solr-morph-transformer
+* https://github.com/culturegraph/solr-metamorph-entity-processor
+
+Add libraries to `solrconfig.xml`
+
+```
+<lib dir="${solr.install.dir:../../../..}/dist/" regex="solr-hashcode-transformer-.*\.jar" />
+<lib dir="${solr.install.dir:../../../..}/dist/" regex="solr-morph-transformer-.*\.jar" />
+<lib dir="${solr.install.dir:../../../..}/dist/" regex="solr-metamorph-entity-processor-.*\.jar" />
+```
+
+## Automatic Installation
+
+The `solr-respin.sh` script modifies a Apache Solr release by doing the following:
+
+* Download Solr release
+* Add the culturegraph config set
+* Add the required add-ons (and dependencies)
+* Modify the `sorlconfig.xml`
+  * Add data import request handler
+  * Include libraries for the required add-ons
+
+
+Run it with:
+
+```
+bash solr-respin.sh
+```
---
+++ solr-respin.sh
@@ -0,0 +1,166 @@
+#!/bin/bash
+# Desc: A script that automates the modification of a vanilla solr release
+
+#
+# Settings
+#
+
+SOLR_VERSION="7.5.0"
+SOLR="solr-${SOLR_VERSION}"
+METAFACTURE_VERSION="5.0.0"
+TARGET=${SOLR}-culturegraph-respin.zip
+
+
+#
+# Welcome
+#
+
+echo "SOLR CULTUREGRAPH RESPIN"
+echo "------------------------"
+
+#
+# Check required commands
+#
+
+if [! which realpath 2> /dev/null ]; then echo "Command 'realpath' not found" && exit 1; fi
+if [! which sed 2> /dev/null ]; then echo "Command 'sed' not found" && exit 1; fi
+if [! which jar 2> /dev/null ]; then echo "Command 'jar' not found" && exit 1; fi
+if [! which wget 2> /dev/null ]; then echo "Command 'wget' not found" && exit 1; fi
+if [! which unzip 2> /dev/null ]; then echo "Command 'unzip' not found" && exit 1; fi
+if [! which zip 2> /dev/null ]; then echo "Command 'zip' not found" && exit 1; fi
+
+#
+# Prepare solr release archive
+#
+
+if [ -f ${TARGET} ]; then
+echo "Removing previous target!"
+rm ${TARGET}
+fi
+
+if [ -d ${SOLR} ]; then
+echo "Removing previous solr directory!"
+rm -rf ${SOLR}
+fi
+
+if [ ! -f ${SOLR}.zip ]; then
+echo "Downloading solr release ${SOLR_VERSION}"
+url="http://ftp.halifax.rwth-aachen.de/apache/lucene/solr/${SOLR_VERSION}/${SOLR}.zip"
+
+echo "Url: ${url}"
+wget -q ${url}
+fi
+
+if [ ! -f ${SOLR}.zip ];
+then echo "Solr release not found!" && exit 1;
+fi
+
+echo "Unzipping archive"
+unzip -q ${SOLR}.zip
+echo "Removing zip file"
+rm ${SOLR}.zip
+
+
+#
+# Add and modify config set
+#
+
+echo "Copying config set"
+if [ ! -d configsets/culturegraph ]; then
+echo "Config set not found!" && exit 1
+fi
+cp -a configsets/culturegraph ${SOLR}/server/solr/configsets
+
+solr=$(realpath ${SOLR})
+solrconfig=${solr}/server/solr/configsets/culturegraph/conf/solrconfig.xml
+
+# Backup vanilla solrconfig.xml
+echo "Creating backup of vanilla solrconfig.xml"
+cp ${solrconfig} ${solrconfig}.bak
+
+echo "Adding libs to solrconfig.xml"
+# Add plugin jars
+# NOTE: We need to mask $ with \$ to please sed
+cat <<-EOF > snippet.txt
+  <lib dir="\${solr.install.dir:../../../..}/dist/" regex="solr-dataimporthandler-.*\.jar" />
+  <lib dir="\${solr.install.dir:../../../..}/dist/" regex="solr-hashcode-transformer-.*\.jar" />
+  <lib dir="\${solr.install.dir:../../../..}/dist/" regex="solr-metamorph-transformer-.*\.jar" />
+  <lib dir="\${solr.install.dir:../../../..}/dist/" regex="solr-metamorph-entity-processor-.*\.jar" />
+EOF
+sed -i '/<!-- Data Directory/i <!-- ADDED LIBS -->' ${solrconfig}
+sed -i '/<!-- ADDED LIBS -->/r snippet.txt' ${solrconfig}
+
+echo "Adding request handler to solrconfig.xml"
+# Add data import request handler
+cat <<-EOF > snippet.txt
+  <requestHandler name="/dataimport" class="solr.DataImportHandler">
+    <lst name="defaults">
+      <str name="config">solr-data-config.xml</str>
+    </lst>
+  </requestHandler>
+EOF
+sed -i '/<!-- Request Handlers/i <!-- ADDED REQUEST HANDLERS -->' ${solrconfig}
+sed -i '/<!-- ADDED REQUEST HANDLERS -->/r snippet.txt' ${solrconfig}
+
+# Remove temporay file
+rm snippet.txt
+
+
+#
+# Modify solr server
+#
+
+# Add third party add-ons for the data import handler (into: dist)
+echo "Adding third-party (data import handler add-ons) jars to dist"
+solr=$(realpath ${SOLR})
+wget -q -P ${solr}/dist https://github.com/culturegraph/solr-hashcode-transformer/releases/download/v0.1.0/solr-hashcode-transformer-0.1.0.jar
+[ ! $? ] && echo "solr-hashcode-transformer-0.1.0.jar not found!" && exit 1
+
+wget -q -P ${solr}/dist https://github.com/culturegraph/solr-metamorph-transformer/releases/download/v0.1.0/solr-metamorph-transformer-0.1.0-fat.jar
+[ ! $? ] && echo "solr-metamorph-transformer-0.1.0-fat.jar not found!" && exit 1
+
+wget -q -P ${solr}/dist https://github.com/culturegraph/solr-metamorph-entity-processor/releases/download/v0.2.0/solr-metamorph-entity-processor-0.2.0-fat.jar
+[ ! $? ] && echo "solr-metamorph-entity-processor-0.2.0-fat.jar not found!" && exit 1
+
+# Add metafacture to solr server (into: server/lib/ext/metafacture)
+echo "Adding third-party (metafacture) jars to solr server"
+lib=$(realpath ${SOLR}/server/lib/ext)
+mkdir -p ${lib}
+
+repo="http://central.maven.org/maven2/org/metafacture"
+modules="metafacture-biblio metafacture-commons metafacture-flowcontrol metafacture-framework metafacture-io metafacture-mangling metamorph metamorph-api"
+for module in $modules; do
+  wget -q -P ${lib} ${repo}/${module}/${METAFACTURE_VERSION}/${module}-${METAFACTURE_VERSION}.jar
+done
+
+# Add additional resources to solr server (into: server/resources)
+echo "Adding resources to solr server"
+res=${SOLR}/server/resources
+cd ${res}
+  jar xf ${lib}/metamorph-${METAFACTURE_VERSION}.jar morph-collectors.properties
+  jar xf ${lib}/metamorph-${METAFACTURE_VERSION}.jar morph-functions.properties
+  jar xf ${lib}/metamorph-${METAFACTURE_VERSION}.jar morph-maps.properties
+cd - > /dev/null
+
+# Add schemata to solr server (into: server/schemata)
+echo "Adding schemata to solr server"
+cd ${SOLR}/server
+jar xf ${lib}/metamorph-${METAFACTURE_VERSION}.jar schemata
+cd - > /dev/null
+
+
+#
+# Create new zip archive
+#
+
+echo "Creating archive ${TARGET}"
+zip -q -r ${TARGET} ${SOLR}
+echo "Removing solr directory"
+rm -rf ${SOLR}
+
+if [ ! -f ${TARGET} ]; then
+echo "Final archive not found!" && exit 1;
+else
+echo "Done"
+fi
+
